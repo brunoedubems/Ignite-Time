e26481a759414cee84c563383874d1a90a98eb98
Bruno <bemds1@aluno.ifnmg.edu.br>
2023-01-13 19:04:29 -0300

    form vali

--- Igite-Time/src/pages/Home/index.tsx
+++ Igite-Time/src/pages/Home/index.tsx
@@ -1,4 +1,7 @@
 import { Play } from "phosphor-react";
+import { useForm} from 'react-hook-form';
+import{zodResolver} from '@hookform/resolvers/zod';
+import * as zod from 'zod';
 import {
   CountdownContainer,
   FormContainer,
@@ -9,15 +12,38 @@ import {
   TaskInput,
 } from "./styles";
 
+const newCycleFormValidationSchema = zod.object({
+  task: zod.string().min(1, 'Informe a tarefa'),
+  MinutesAmount: zod
+  .number()
+  .min(5,'O ciclo precisa ser no mínimo de 5 minutos.')
+  .max(60, 'O ciclo precisa ser no máximo de 60 minutos.'),
+})
+
 export function Home() {
+const {register, handleSubmit, watch, formState} = useForm({
+  resolver: zodResolver(newCycleFormValidationSchema),
+})
+
+
+function handleCreateNewCycle(data: any){
+
+}
+
+const task = watch('task')
+const isSubmitDisabled = !task
+
+
   return (
     <HomeContainer>
-      <form action="">
+      <form onSubmit={handleSubmit(handleCreateNewCycle)} action="">
         <FormContainer>
           <label htmlFor="task">Vou trabalhar em</label>
-          <TaskInput id="task" 
+          <TaskInput 
+          id="task" 
           placeholder="Dê um nome para seu projeto"
           list="task-suggestions"
+          {...register('task')}
           />
 
             <datalist id="task-suggestions">
@@ -34,6 +60,7 @@ export function Home() {
             step={0}
             min={0}
             max={60}
+            {...register('minutesAmount',{valueAsNumber: true})}
           />
           <span>minutos.</span>
         </FormContainer>
@@ -46,7 +73,7 @@ export function Home() {
           <span>0</span>
         </CountdownContainer>
 
-        <StartCountdownButton disabled type="submit">
+        <StartCountdownButton disabled={isSubmitDisabled} type="submit">
           <Play size={24} />
           Começar
         </StartCountdownButton>
